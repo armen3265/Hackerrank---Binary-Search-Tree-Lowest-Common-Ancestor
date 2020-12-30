06405bc9137304582dfd00397461a257f05e7478
armen3265 <74138554+armen3265@users.noreply.github.com>
2020-12-30 12:27:45 +0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+
+/*The tree node has data, left child and right child 
+class Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+*/
+  
+    Node *lca(Node *root, int v1,int v2) {
+		// Write your code here.
+        Node* temp = root;
+        int bigInt, smallInt;
+        if(v1>v2)
+        {
+            bigInt = v1;
+            smallInt = v2;
+        }
+        else {
+            smallInt = v1;
+            bigInt = v2;
+        }
+
+        while(temp->data != bigInt && temp->data != smallInt)
+        {
+            if(temp->data > smallInt && temp->data < bigInt)
+            {
+                return temp;
+            }
+            if(temp->data < smallInt)
+            {
+                temp = temp->right;
+            }
+            else {
+                temp = temp->left;
+            }
+        }
+        return temp;
+    }
+
+}; //End of Solution
+
+int main() {
+  
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  	
+  	int v1, v2;
+  	std::cin >> v1 >> v2;
+  
+    Node *ans = myTree.lca(root, v1, v2);
+    
+  	std::cout << ans->data;
+
+    return 0;
+}
